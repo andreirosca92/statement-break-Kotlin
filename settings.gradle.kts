75c41a9a51a869173398bcc88e9a8ad910f21bee
andrei <andreirosca92@gmail.com>
2023-04-17 11:32:49 +0200

    Kotlin break example

---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "Jumping_Statement_break"
+
---
+++ src/nativeMain/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args:Array<String>) {
+    for(i in 1..5){ // stampa fino a 3 a causa del break
+        if(i==3) {
+            break
+        }
+        println(i) //stampa 1 e 2
+    }
+}
